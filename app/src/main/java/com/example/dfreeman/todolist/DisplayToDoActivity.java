67e3b14d4ff12f17a796af8218c07dde60237dc0
dfreemanRIIS <dfreeman@riis.com>
2017-01-12 10:36:09 -0500

    Create basic DisplayToDoActivity for first list item

---
+++ app/src/main/java/com/example/dfreeman/todolist/DisplayToDoActivity.java
@@ -0,0 +1,13 @@
+package com.example.dfreeman.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_to_do);
+    }
+}
--- app/src/main/java/com/example/dfreeman/todolist/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/todolist/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.dfreeman.todolist;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.view.View;
@@ -17,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         ListView listToDos = (ListView)findViewById(R.id.toDoList);
         ArrayAdapter<toDoItem> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, toDoItem.testToDo);
         listToDos.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, DisplayToDoActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        listToDos.setOnItemClickListener(itemClickListener);
     }
 
     public void onAddNewActivity(View view) {
