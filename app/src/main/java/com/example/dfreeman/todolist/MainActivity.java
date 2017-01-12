b2fa836ea7bc6ba7e405dc4e60126027eeba6043
dfreemanRIIS <dfreeman@riis.com>
2017-01-12 09:02:11 -0500

    Wire up basic AddNew page

--- app/src/main/java/com/example/dfreeman/todolist/MainActivity.java
+++ app/src/main/java/com/example/dfreeman/todolist/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
         ListView listToDos = (ListView)findViewById(R.id.toDoList);
         ArrayAdapter<toDoItem> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, toDoItem.testToDo);
         listToDos.setAdapter(listAdapter);
+    }
+
+    public void onAddNewActivity(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
     }
 }
