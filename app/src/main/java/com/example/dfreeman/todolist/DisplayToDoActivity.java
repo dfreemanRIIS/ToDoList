23d7928809c392f1b599da9652e951937ab59d24
dfreemanRIIS <dfreeman@riis.com>
2017-01-12 10:59:39 -0500

    Clicking on a ToDo brings up basic info page(needs work)

--- app/src/main/java/com/example/dfreeman/todolist/DisplayToDoActivity.java
+++ app/src/main/java/com/example/dfreeman/todolist/DisplayToDoActivity.java
@@ -1,13 +1,19 @@
 package com.example.dfreeman.todolist;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class DisplayToDoActivity extends AppCompatActivity {
+public class DisplayToDoActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_to_do);
+        //setContentView(R.layout.activity_display_to_do);
+        ListView listView = getListView();
+        ArrayAdapter<toDoItem> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, toDoItem.testToDo);
+        listView.setAdapter(listAdapter);
     }
 }
