d4c8158fac653ff9acd1e44872f2ab5bcb1a1445
dfreemanRIIS <dfreeman@riis.com>
2017-01-12 09:35:13 -0500

    Add Date and TimePickers

--- app/src/main/java/com/example/dfreeman/todolist/AddNewActivity.java
+++ app/src/main/java/com/example/dfreeman/todolist/AddNewActivity.java
@@ -2,6 +2,8 @@ package com.example.dfreeman.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v4.app.DialogFragment;
 
 public class AddNewActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class AddNewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
